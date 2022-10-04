8c18359f106891fb072230f8e521291ba50e0cd0
Abhishek <abhi9811206369@gmail.com>
2022-10-04 19:09:05 +0530

    boilerplate for fetching user data and states

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { useGetUsers } from './hooks/useGetUsers'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { data, isLoading, isError } = useGetUsers()
+
+  let content: JSX.Element | null
+
+  if (isLoading) {
+    content = <div>Loading...</div>
+  } else if (isError && !data) {
+    content = <div>Something went wrong!</div>
+  } else {
+    content = <div>
+      got users!
+    </div>
+  }
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Contacts
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {content}
     </div>
   )
 }
